c93d8ad536e21194d8bd03a5bde10c088ba01681
dibk <dibk_100@naver.com>
2025-06-04 17:30:34 +0900

    re-setting folder

--- task02_regression/main_regression.py
+++ task02_regression/main_regression.py
@@ -8,7 +8,7 @@ import os
 
 def main():
     load_dotenv(verbose=True)               # .env심기, wandb로그인 key
-    parser = argparse.ArgumentParser(description="BERT-based multi-label classification")
+    parser = argparse.ArgumentParser(description="BERT-based Heat input Regression")
     parser.add_argument('--mode', choices=['train', 'eval'], required=True)
     parser.add_argument('--config', type=str, required=True, help='Path to config YAML')
     args = parser.parse_args()
@@ -30,8 +30,10 @@ def main():
         wandb.finish()
         
     elif args.mode == 'eval':
-        test_loss, test_macro_f1, test_micro_f1 = evaluate_model(config, split='test')
-        print("Test Macro F1:", test_macro_f1)
+        test_mse, test_rmse, test_r2 = evaluate_model(config, split='test')
+        print(f"✅ Test MSE: {test_mse:.4f}")
+        print(f"✅ Test RMSE: {test_rmse:.4f}")
+        print(f"✅ Test R²: {test_r2:.4f}")
     
     else :
         assert False, "다시 코드 실행"
